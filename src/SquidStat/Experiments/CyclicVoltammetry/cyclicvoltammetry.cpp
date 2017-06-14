a9a2d8d492874e5a2e477e86df7c42905ad7d112
mdion1 <matt@admiralinstruments.com>
2017-06-14 16:12:56 -0700

    Added calculations for slew rate

--- src/SquidStat/Experiments/CyclicVoltammetry/cyclicvoltammetry.cpp
+++ src/SquidStat/Experiments/CyclicVoltammetry/cyclicvoltammetry.cpp
@@ -22,10 +22,10 @@
 
 
 QString CyclicVoltammetry::GetShortName() const {
-	return "Cyclic Voltammetry";
+	return "Basic Cyclic Voltammetry";
 }
 QString CyclicVoltammetry::GetFullName() const {
-	return "Cyclic Voltammetry";
+	return "Basic Cyclic Voltammetry";
 }
 QString CyclicVoltammetry::GetDescription() const {
 	return "This experiment sweeps the potential of the working electrode back and forth between <b>upper potential</b> and <b>lower potential</b> at a constant <b>scan rate dE/dT</b> for a specified number of <b>cycles</b>.";
@@ -137,7 +137,7 @@ QByteArray CyclicVoltammetry::GetNodesData(QWidget *wdg, const CalibrationData &
 	exp.isTail = false;
 	exp.nodeType = DCNODE_POINT_POT;
 	exp.tMin = 1e7;
-	exp.tMax = 2e8;
+	exp.tMax = 1e8;
 	exp.samplingParams.ADCTimerDiv = 2;
 	exp.samplingParams.ADCTimerPeriod = 200000;
 	exp.samplingParams.ADCBufferSizeEven = 20;
@@ -145,7 +145,7 @@ QByteArray CyclicVoltammetry::GetNodesData(QWidget *wdg, const CalibrationData &
 	exp.samplingParams.DACMultEven = 20;
 	exp.samplingParams.DACMultOdd = 20;
 	exp.samplingParams.PointsIgnored = 0;
-	exp.DCPoint_pot.VPointUserInput = startVoltage;
+	exp.DCPoint_pot.VPointUserInput = (int)(startVoltage * 3276.8);
 	exp.DCPoint_pot.VPointVsOCP = false;
 	exp.DCPoint_pot.Imax = 32767;
 	exp.DCPoint_pot.IrangeMax = RANGE0;
@@ -159,16 +159,10 @@ QByteArray CyclicVoltammetry::GetNodesData(QWidget *wdg, const CalibrationData &
 	exp.nodeType = DCNODE_SWEEP_POT;
 	exp.tMin = 100000;
 	exp.tMax = 0xFFFFFFFFFFFFFFFF;
-	exp.samplingParams.ADCTimerDiv = 2;
-	exp.samplingParams.ADCTimerPeriod = 100000;
-	exp.samplingParams.ADCBufferSizeEven = 10;
-	exp.samplingParams.ADCBufferSizeOdd = 10;
-	exp.samplingParams.DACMultEven = 10;
-	exp.samplingParams.DACMultOdd = 10;
-	exp.samplingParams.PointsIgnored = 0;
-	exp.DCSweep_pot.VStartUserInput = startVoltage;
+	getSlewParameters(dEdt/1000, &exp);
+	exp.DCSweep_pot.VStartUserInput = (int)(startVoltage * 3276.8);
 	exp.DCSweep_pot.VStartVsOCP = false;
-	exp.DCSweep_pot.VEndUserInput = upperVoltage;
+	exp.DCSweep_pot.VEndUserInput = (int)(upperVoltage * 3276.8);
 	exp.DCSweep_pot.VEndVsOCP = false;
 	exp.DCSweep_pot.VStep = 1;
 	exp.MaxPlays = 1;
@@ -179,17 +173,9 @@ QByteArray CyclicVoltammetry::GetNodesData(QWidget *wdg, const CalibrationData &
 	exp.nodeType = DCNODE_SWEEP_POT;
 	exp.tMin = 100000;
 	exp.tMax = 0xFFFFFFFFFFFFFFFF;
-	exp.samplingParams.ADCTimerDiv = 2;
-	exp.samplingParams.ADCTimerPeriod = 100000;
-	exp.samplingParams.ADCBufferSizeEven = 10;
-	exp.samplingParams.ADCBufferSizeOdd = 10;
-	exp.samplingParams.DACMultEven = 10;
-	exp.samplingParams.DACMultOdd = 10;
-	exp.samplingParams.PointsIgnored = 0;
-	//exp.samplingParams.isDACStatic = false;
-	exp.DCSweep_pot.VStartUserInput = upperVoltage;
+	exp.DCSweep_pot.VStartUserInput = (int)(upperVoltage * 3276.8);
 	exp.DCSweep_pot.VStartVsOCP = false;
-	exp.DCSweep_pot.VEndUserInput = lowerVoltage;
+	exp.DCSweep_pot.VEndUserInput = (int)(lowerVoltage * 3276.8);
 	exp.DCSweep_pot.VEndVsOCP = false;
 	exp.DCSweep_pot.VStep = 1;
 	exp.MaxPlays = 1;
@@ -201,16 +187,9 @@ QByteArray CyclicVoltammetry::GetNodesData(QWidget *wdg, const CalibrationData &
 	exp.nodeType = DCNODE_SWEEP_POT;
 	exp.tMin = 100000;
 	exp.tMax = 0xFFFFFFFFFFFFFFFF;
-	exp.samplingParams.ADCTimerDiv = 2;
-	exp.samplingParams.ADCTimerPeriod = 100000;
-	exp.samplingParams.ADCBufferSizeEven = 10;
-	exp.samplingParams.ADCBufferSizeOdd = 10;
-	exp.samplingParams.DACMultEven = 10;
-	exp.samplingParams.DACMultOdd = 10;
-	exp.samplingParams.PointsIgnored = 0;
-	exp.DCSweep_pot.VStartUserInput = lowerVoltage;
+	exp.DCSweep_pot.VStartUserInput = (int)(lowerVoltage * 3276.8);
 	exp.DCSweep_pot.VStartVsOCP = false;
-	exp.DCSweep_pot.VEndUserInput = upperVoltage;
+	exp.DCSweep_pot.VEndUserInput = (int)(upperVoltage * 3276.8);
 	exp.DCSweep_pot.VEndVsOCP = false;
 	exp.DCSweep_pot.VStep = 1;
 	exp.MaxPlays = cycles;
@@ -220,4 +199,63 @@ QByteArray CyclicVoltammetry::GetNodesData(QWidget *wdg, const CalibrationData &
 	PUSH_NEW_NODE_DATA();
 
 	NODES_DATA_END();
+}
+
+void CyclicVoltammetry::getSlewParameters(double dVdt, ExperimentNode_t * pNode) const
+{
+	/* This switch-case is a placeholder for calculating dt_min, which needs to be defined elsewhere*/
+	int dt_min = 1;
+	int HardwareVersion = 0;
+	switch (HardwareVersion)
+	{
+		case 0:
+			dt_min = 50000; //500 microseconds * 100 ticks/microsecond
+			break;
+		case 1:
+			dt_min = 500;	//5 microseconds * 100 ticks/microsecond
+			break;
+		default:
+			break;
+	}
+	pNode->samplingParams.ADCTimerDiv = 1;
+	pNode->samplingParams.DACMultEven = pNode->samplingParams.DACMultOdd = 1;
+
+	/* 1) Minimize dt, maximize DACMult*/
+	uint32_t dt;
+	do
+	{
+		dt = (uint32_t)(1 / dVdt * 1e8 / 3276.8 / pNode->samplingParams.DACMultEven);		//3276.8 is a placeholder for cal->m_DAC,V
+		if (dt / dt_min > 1)
+		{
+			pNode->samplingParams.DACMultEven <<= 1;
+			pNode->samplingParams.DACMultOdd <<= 1;
+		}
+	} while (dt / dt_min > 1);
+
+	/* 2) Increase VStep, if necessary */
+	while (dt < dt_min)
+	{
+		pNode->DCSweep_pot.VStep++;
+		dt = (uint32_t)(1 / dVdt * 1e8 / 3276.8 / pNode->samplingParams.DACMultEven * pNode->DCSweep_pot.VStep);
+	}
+
+	/* 3) Calculate ADCMult */
+	pNode->samplingParams.ADCBufferSizeEven = pNode->samplingParams.ADCBufferSizeOdd = pNode->samplingParams.DACMultEven;
+	while (pNode->samplingParams.ADCBufferSizeEven * dt > 1e8)
+	{
+		pNode->samplingParams.ADCBufferSizeEven >>= 1;
+		pNode->samplingParams.ADCBufferSizeOdd >>= 1;
+	}
+	if (pNode->samplingParams.ADCBufferSizeEven == pNode->samplingParams.DACMultEven)
+		pNode->samplingParams.PointsIgnored = pNode->samplingParams.ADCBufferSizeEven / 2;
+
+	pNode->samplingParams.ADCTimerDiv = 1;
+	int timerDiv = 1;
+	pNode->samplingParams.ADCTimerPeriod = dt;
+	while (pNode->samplingParams.ADCTimerPeriod > 2147483648)
+	{
+		pNode->samplingParams.ADCTimerDiv++;
+		timerDiv <<= 1;
+		pNode->samplingParams.ADCTimerPeriod = dt / timerDiv;
+	} 
 }
