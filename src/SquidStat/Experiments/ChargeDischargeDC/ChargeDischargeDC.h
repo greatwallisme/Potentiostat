310e570c0f904970a2bb8319ac46d0827e422250
mdion1 <matt@admiralinstruments.com>
2017-06-26 14:26:54 -0700

    some changes

--- src/SquidStat/Experiments/ChargeDischargeDC/ChargeDischargeDC.h
+++ src/SquidStat/Experiments/ChargeDischargeDC/ChargeDischargeDC.h
@@ -23,8 +23,10 @@ public:
 	void SaveData(QFile&, const DataMap&) const;
 private:
 	int hardwareVersion; //TODO: this is a placeholder member
-	void getSlewParameters(double dVdt, ExperimentNode_t * pNode) const;
-
+	void getSamplingParameters(double sampling_interval, ExperimentNode_t * pNode) const;
+	currentRange_t getCurrentRange(double current, const CalibrationData * cal, HardwareModel_t hwModel) const;
+	int16_t getCurrentBinary(currentRange_t range, double current, const CalibrationData * cal) const;
+	int16_t getVoltageBinary(double voltage, const CalibrationData * cal) const;
 };
 
 #endif // CHARGE_DISCHARGE_DC_H
